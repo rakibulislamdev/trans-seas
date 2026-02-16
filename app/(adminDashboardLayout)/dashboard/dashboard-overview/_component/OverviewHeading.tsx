f4e90230d46b53daa0e54a691628cf74e3c5608e
alok4d <alokroy602701@gmail.com>
2026-02-16 18:19:58 +0600

    feat: Add `OverviewHeading` component to display a personalized greeting and overview message.

--- app/(adminDashboardLayout)/dashboard/dashboard-overview/_component/OverviewHeading.tsx
+++ app/(adminDashboardLayout)/dashboard/dashboard-overview/_component/OverviewHeading.tsx
@@ -10,10 +10,6 @@ export default function OverviewHeading() {
                     Here's what's happening with your procurement activities
                 </p>
             </div>
-            <button className="flex items-center gap-2 bg-brand-primary hover:opacity-90 text-text-strong-white px-5 py-2.5 rounded-xl font-bold text-18 transition-all shadow-sm cursor-pointer">
-                <FolderLock size={21} />
-                New Project
-            </button>
         </div>
     )
 }
