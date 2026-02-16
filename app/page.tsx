7c2feac82c07c3bdf11bc62f0f87170e215ad03f
rakibulislamdev <rirakib03@gmail.com>
2026-02-16 18:45:22 +0600

    solved

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import DashboardLayout from "./(adminDashboardLayout)/layout";
-
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <DashboardLayout />
-
+    <main className="min-h-screen flex items-center justify-center bg-background">
+      <Link
+        href="/dashboard"
+        className="px-6 py-3 bg-blue-600 text-white rounded-lg font-medium hover:bg-blue-700 transition"
+      >
+        Home page is under development. Click here to view the Dashboard
+      </Link>
+    </main>
   );
 }
